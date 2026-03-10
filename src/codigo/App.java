d809071911da253ef460ecbf90cf861f358b8f21
estud <estud@Daniela.home>
2026-03-10 13:33:36 +0100

    "Añadida la clase App"

---
+++ src/codigo/App.java
@@ -0,0 +1,11 @@
+package codigo;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Este archivo lanzará la aplicación");
+	}
+
+}
