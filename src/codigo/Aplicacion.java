13adc9609a99766ceb63ab3adb68a14c29ac03cd
estud <estud@Daniela.home>
2026-03-10 13:36:00 +0100

    "Renombrar clase e imprimir mi nombre"

--- src/codigo/Aplicacion.java
+++ src/codigo/Aplicacion.java
@@ -1,11 +1,12 @@
 package codigo;
 
-public class App {
+public class Aplicacion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("Este archivo lanzará la aplicación");
+		System.out.println("Stephany Díaz");
 	}
 
 }
